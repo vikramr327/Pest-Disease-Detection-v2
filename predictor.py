d88ca89206e3561a66a304d77377eaf6f2a3aafb
vikramr327 <vikramranand@gmail.com>
2025-03-11 19:12:16 -0700

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 # print(categories[pred])
 def make_prediction(input_image):
-  myModel = load('DiseaseDetectionEpoch7.joblib')
+  myModel = load('DiseaseDetectionEpoch73.joblib')
   #myModel.eval()  # Set model to evaluation mode #temp
 #  st.write ("Model uploaded!") # You may remove this in your finalized web app!
   input_resized = cv2.resize(input_image, (128, 128))
