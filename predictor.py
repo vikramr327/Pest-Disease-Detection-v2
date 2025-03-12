e223e14f093a592fcdc5a577894ec7c4f9b7a0c9
vikramr327 <vikramranand@gmail.com>
2025-03-11 20:35:38 -0700

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -7,11 +7,11 @@ from joblib import load
 import streamlit as st
 # ... your existing code ...
 
-
+torch.serialization.add_safe_globals([ResNet])
 # print(categories[pred])
 def make_prediction(input_image):
   #myModel = torch.load('DiseaseDetectionEpoch73.pth')
-  myModel = torch.load("DiseaseDetectionEpoch73.pth", weights_only=False)
+  myModel = torch.load("DiseaseDetectionEpoch73.pth")
   #myModel.eval()  # Set model to evaluation mode #temp
 #  st.write ("Model uploaded!") # You may remove this in your finalized web app!
   input_resized = cv2.resize(input_image, (128, 128))
